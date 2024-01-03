6c557130701a3a67d77337bf43fac99ebae57429
Preethi867 <155513861+Preethi867@users.noreply.github.com>
2024-01-03 12:30:59 +0530

    Update Student.java

--- core_java/Student.java
+++ core_java/Student.java
@@ -2,6 +2,7 @@ class Student{
     public static void main(String args[]){
         System.out.println("Hi");
         System.out.print("hello");
+        System.out.print("hello again");
     }
 }
 
